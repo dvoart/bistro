5455793806b74fa4bbee10bb339960d011f9b29e
David Arutunian <157921448+dvoart@users.noreply.github.com>
2024-04-02 22:06:22 -0700

    Add files via upload

---
+++ bistros/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>BOB'S BBQ | Dallas</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+<link href='http://fonts.googleapis.com/css?family=Oleo+Script' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css2?family=Girassol&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap" rel="stylesheet">
+
+
+<!-- Custom styles for this template -->
+<style>
+body {
+padding-top: 54px;
+}
+@media (min-width: 992px) {
+body {
+padding-top: 56px;
+	}
+}
+h3 {
+font: 300 30px/1.3 'Oleo Script', Helvetica, sans-serif;
+color: #2b2b2b;  
+}
+.navbar-brand {
+
+font: 200 30px/1.3 'Oleo Script', Helvetica, sans-serif;
+color: #ffffff; 
+}
+span {
+font: 200 17px/1.3 Helvetica, sans-serif;
+color: #2b2b2b; 
+}
+.bg-light { background-color: #ED9121 !important; }
+a { color: #000000; }
+a:hover { color: #000000; }
+.nav-link { color: #000 !important; }
+
+</style>
+
+</head>
+
+<body style="background-image: url(images/bbqback.jpg);">
+  
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top container">
+  <div class="container">
+	<a class="navbar-brand" href="#">BOB'S BBQ <span style="font-family: 'Protest Riot', sans-serif;">Dallas, TX</span></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+	  <ul class="navbar-nav ml-auto">
+		<li class="nav-item">
+		  <a class="nav-link" href="#">Reservations</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="#">Jobs</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="#"><i class="fa fa-star" aria-hidden="true"></i> Contact Us</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="tel:1-800-123-4567" class="lead"><strong>1-800-123-4567</strong></a>
+		</li>
+	  </ul>
+	</div>
+  </div>
+</nav>
+    
+   
+
+<!-- Page Content -->
+   
+<div class="container text-center"  style="background-image: url(images/bistros.jpg);color:white;background-repeat: no-repeat;background-color:black;height:543px;width:100%;">
+      
+      
+<div class="row">
+  <div class="col-lg-12 text-center">
+	<h1 style="padding-top: 30px; font-family: 'Protest Riot', sans-serif;">Voted Best New Restaurant in Dallas!</h1>
+
+	<p style="padding-top:200px;"><button type="button" class="btn btn-dark">Order Online</button></p>
+	<br><br>
+  </div>
+</div>
+</div> <!--/container-->
+    
+    
+<div class="container" style="background-color:#FFA500;opacity: 0.9;">
+
+<div class="row">
+<div class="col-lg-12 text-center"><br>
+  <h3>Our Menus</h3><br>
+</div>
+ <br><br>
+<div class="col-lg-4 text-center">
+  <img src="images/breakfast.jpg" class="img-thumbnail">
+  <br><br><a href="#" class="btn btn-dark">Breakfast</a><br><br>
+</div>
+<div class="col-lg-4 text-center">
+  <img src="images/lunch.jpg" class="img-thumbnail">
+  <br><br><a href="#" class="btn btn-dark">Lunch</a><br><br>
+</div>
+<div class="col-lg-4 text-center">
+  <img src="images/dinner.jpg" class="img-thumbnail">
+  <br><br><a href="#" class="btn btn-dark">Dinner</a><br><br>
+</div>
+  
+<br><br>
+</div> <!--/row-->
+<br><br>
+</div> <!--/container-->
+    
+    
+    
+<div class="container" style="background-color:#ffffff;">
+      
+<div class="row">
+      
+<div class="col-lg-6"><br>
+<h3>Our Pitmaster: Bob Smokes <i class="fa fa-id-card" aria-hidden="true" style="color:gold;text-shadow: 1px 1px black;font-size:28px;"></i></h3><br>
+<p class="lead" style=""><img src="images/chef.jpg" class="img-thumbnail" style="padding:5px;width:150px;float:right;">Bob was born in Fort Worth, TX. He was raised in Dallas, TX and attended the Lone State Culinary Institute.
+<br><br>
+If hes not at work, you’ll find him cheering on the  Dallas Stars or spending time with his dog, Max. He considers Gordan Ramsey his hero.
+<br><br>
+Bob has been working in the restaurant industry for a very long time and really enjoys the bbq scene.<br>
+<br><i class="fa fa-twitter" aria-hidden="true"></i><img src="images/socialicons.jpg"></p>
+</div>
+        
+<div class="col-lg-6" style="background-color:#F8F8F8;border-left:1px dashed gray;"><br>
+<h3>Critical Acclaim</h3><br>
+<p class="lead" style="text-align: justify;"><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> "Ok, this is the third time I have had BBQ here, it is amazing"<br><span style="color:#30a64a;font-size:14px;float:right;">--The Dallas Sun</span></p>
+<br><br>
+
+<p class="lead" style="text-align: justify;"><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> "Bob's BBQ is the best in the world." <br><span style="color:#30a64a;font-size:14px;float:right;">--The Phantom Meats</span></p>
+
+<br><br>
+            
+</div> <!--/col-->
+</div> <!--/row-->
+</div> <!--/container-->
+    
+<div class="container" style="background-color:#FFA824;color:gray;opacity: 0.81;">
+      
+      
+<div class="row">
+<div class="col">
+<p class="text-left" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;">&copy; Copyright <?=date('Y')?>, Bob's BBQ</p>
+</div>
+
+<div class="col">
+<p class="text-lg-center" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;">Bob's BBQ, 20 BBQ St., Dallas, TX 32222</p>
+</div>
+
+<div class="col">
+<p class="text-lg-right" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+</div>
+
+
+</div> <!--/row-->
+</div> <!--/container-->
+
+
+
+    
+    
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
